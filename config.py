11bf79996b7bbf756451282ace36a8938206ad76
Shodik12 <67968446+Shodik12@users.noreply.github.com>
2022-03-27 07:15:54 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", "")
+ADMIN = int(os.environ.get("ADMIN", ""))
+OWNER = os.environ.get("OWNER", "kenkanasw")
+GROUP = os.environ.get("GROUP", "musikkugroup")
+CHANNEL = os.environ.get("CHANNEL", "musikkuchannel")
+PROJECT_NAME = os.environ.get("PROJECT_NAME", "PRIVATE CHAT")
