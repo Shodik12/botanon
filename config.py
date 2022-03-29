44092eabb4f69c223ffa085c80688c10761b94e4
Shodik12 <67968446+Shodik12@users.noreply.github.com>
2022-03-29 22:30:15 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 TOKEN = os.environ.get("TOKEN", "")
 ADMIN = int(os.environ.get("ADMIN", ""))
 OWNER = os.environ.get("OWNER", "kenkanasw")
-GROUP = os.environ.get("GROUP", "musikkugroup")
+GROUP = os.environ.get("GROUP", "finsq")
 CHANNEL = os.environ.get("CHANNEL", "musikkuchannel")
 PROJECT_NAME = os.environ.get("PROJECT_NAME", "PRIVATE CHAT")
